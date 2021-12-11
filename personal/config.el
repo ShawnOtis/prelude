0bb609d9c94d86e9175e9a403885c0e2d69e69cf
Shawn Otis <otisshawn@gmail.com>
2021-12-10 21:03:09 -0500

    Adds personal configs

---
+++ personal/config.el
@@ -0,0 +1,6 @@
+(disable-theme 'zenburn)
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "DONE")))
+(setq ivy-use-selectable-prompt t)
+(global-set-key "\C-s" 'isearch-forward)
+(global-set-key "\M-x" 'execute-extended-command)
